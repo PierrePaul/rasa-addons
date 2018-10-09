85572dc0a8e04d981de363f703e6dfee6ceb99e8
Ismael <bernierismael@gmail.com>
2018-10-09 00:58:36 -0400

    [PATTERN_BREAKER] add pattern breaker power to the superagent's potential rules

---
+++ rasa_addons/superagent/pattern_breaker.py
@@ -0,0 +1,61 @@
+import io
+import yaml
+import re
+from rasa_core.actions.action import Action
+from rasa_core.events import ActionExecuted, ActionReverted
+
+# This added functionality ensures that when the bot encounters an intent, regardless of conversational flow, it skips to a specific topic. Important to ensure immediate response to urgent matters regardless of training.
+# after intent, then utter.
+# Format in rules.yml file
+# pattern_breaker:
+#  - after: help
+#    then: utter_help_intro
+class PatternBreaker(object):
+    def __init__(self, rules):
+        self.rules = rules if rules is not None else []
+        self.actions_to_ignore = ['action_listen', 'pattern_breaker_utterance']
+
+    def ignore_action(self, action_name):
+        self.actions_to_ignore.append(action_name)
+
+    def find(self, after):
+        for rule in self.rules:
+            if re.match(rule['after'], after):
+                return rule
+        return None
+
+    def get_pattern_breaker(self, parse_data, tracker):
+        intent = parse_data["intent"]["name"]
+        if intent is None:
+            return None
+        return self._get_pattern_breaker_template(parse_data, intent)
+
+    def _get_pattern_breaker_template(self, parse_data, intent):
+        rule = self.find(intent)
+        if rule is None:
+            return None
+        if rule['then'] is None:
+            # ToDo for now, simply ensuring that the info exists, not validating it is an actual template... to be honest, not clear how that works yet. Will implement when I do.
+            return None
+        return rule['then']
+
+    @staticmethod
+    def _load_yaml(rules_file):
+        with io.open(rules_file, 'r', encoding='utf-8') as stream:
+            try:
+                return yaml.load(stream)
+            except yaml.YAMLError as exc:
+                raise ValueError(exc)
+
+
+class PatternBreakerUtterance(Action):
+    def __init__(self, template):
+        self.template = template
+
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_template(self.template, tracker)
+
+        return []
+
+    def name(self):
+        return 'pattern_breaker_utterance'
--- rasa_addons/superagent/rules.py
+++ rasa_addons/superagent/rules.py
@@ -8,7 +8,9 @@ from rasa_core.events import ActionExecuted
 
 from rasa_addons.superagent.disambiguator import Disambiguator
 from rasa_addons.superagent.input_validator import InputValidator
+from rasa_addons.superagent.pattern_breaker import PatternBreaker
 
+from rasa_addons.superagent.pattern_breaker import PatternBreakerUtterance
 from rasa_addons.superagent.input_validator import ActionInvalidUtterance
 logger = logging.getLogger(__name__)
 
@@ -20,7 +22,8 @@ class Rules(object):
         self.allowed_entities = data["allowed_entities"] if data and "allowed_entities" in data else {}
         self.intent_substitutions = data["intent_substitutions"] if data and "intent_substitutions" in data else []
         self.input_validation = InputValidator(data["input_validation"]) if data and "input_validation" in data else []
-        self.disambiguation_policy = Disambiguator(data.get("disambiguation_policy", None) if data else None, 
+        self.pattern_breaker = PatternBreaker(data["pattern_breaker"]) if data and "pattern_breaker" in data else []
+        self.disambiguation_policy = Disambiguator(data.get("disambiguation_policy", None) if data else None,
                                                    data.get("fallback_policy", None) if data else None)
 
     def interrupts(self, dispatcher, parse_data, tracker, run_action):
@@ -40,9 +43,20 @@ class Rules(object):
                 self._utter_error_and_roll_back(dispatcher, tracker, error_template, run_action)
                 return True
 
+        if self.pattern_breaker:
+            pattern_breaker_template = self.pattern_breaker.get_pattern_breaker(parse_data, tracker)
+            if pattern_breaker_template is not None:
+                self._utter_pattern_breaker_template(dispatcher, tracker, pattern_breaker_template, run_action)
+                return True
+
     @staticmethod
     def _utter_error_and_roll_back(dispatcher, tracker, template, run_action):
         action = ActionInvalidUtterance(template)
+        run_action(action, tracker, dispatcher)
+
+    @staticmethod
+    def _utter_pattern_breaker_template(dispatcher, tracker, template, run_action):
+        action = PatternBreakerUtterance(template)
         run_action(action, tracker, dispatcher)
 
     def filter_entities(self, parse_data):
